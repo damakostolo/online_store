4ce5c2a91de545583837e440285c4a4910dbe1a7
DamaKostolo <teodaniil2@gmail.com>
2023-11-10 10:48:50 +0200

    router

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,8 +7,8 @@ const typeRouter = require('./typeRouter')
 
 
 router.use('/user', userRouter)
-router.use('/tupe', typeRouter)
+router.use('/type', typeRouter)
 router.use('/brand', brandRouter)
 router.use('/device', deviceRouter)
-
+ 
 module.exports = router
