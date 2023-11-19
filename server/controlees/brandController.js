ddea49e63c7d4ad43340d6b159032ff33f3415eb
DamaKostolo <teodaniil2@gmail.com>
2023-11-19 23:35:57 +0200

    новые моудли контролеры и гет и пост запросы

--- server/controlees/brandController.js
+++ server/controlees/brandController.js
@@ -1,11 +1,18 @@
-class brandController{
-    async create(req,res){
+const {Brand} = require ('../models/models') 
+const ApiError = require ('../error/ApiError') 
 
+class BrandController{
+    async create(req,res){
+        const {name} = req.body
+        const brand = await Brand.create({name})
+        
+        return res.json(brand)
     }
 
     async getAll (req, res){
-
+        const brands = await Brand.findAll()
+        return res.json(brands)
     }
 }
 
-module.exports = new brandController()
+module.exports = new BrandController()
--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ const express = require('express')  // импортируем некий мод�
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middeleware/ErrorHandlingMiddleware')
+const path = require ('path')
 
 const PORT = process.env.PORT || 5000 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 //Проверка ошибок
